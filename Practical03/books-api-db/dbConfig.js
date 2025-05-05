eb2e64be4489a4c4bec09c8fccc10ff9a23fa5ef
Ahmad Danial Bin Azman <hafizkecil95@gmail.com>
2025-05-05 11:13:15 +0800

    Practical03 Part 1

---
+++ Practical03/books-api-db/dbConfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+    user: "BED_Ahmad", // Replace with your SQL Server login username
+    password: "Pa$$w0rd1", // Replace with your SQL Server login password
+    server: "localhost",
+    database: "bed_db",
+    trustServerCertificate: true,
+    options: {
+      port: 1433, // Default SQL Server port
+      connectionTimeout: 60000, // Connection timeout in milliseconds
+    },
+  };
